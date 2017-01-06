2033649bb0604e2edd8fc16956e595564b026a93
tinysong <tinysong1226@gmail.com>
2017-01-06 13:44:55 +0800

    add reload go-path function

---
+++ layers/ts/ts-lang/funcs.el
@@ -0,0 +1,21 @@
+;;; funcs.el --- tinysong Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2015-2016 tinysong
+;;
+;; Author: tinysong <guanghui8827@gmail.com>
+;; URL: https://github.com/tinysong/spacemacs-private
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+(require 'cl-lib)
+(require 'ido)
+
+;;  reload GOPATH for go
+(defun ts-lang/load-docker-gopath ()
+  (interactive "P")
+  (progn
+    (setq gopath (getenv "GOPATH")
+          vendor (concat  gopath "/src/github.com/docker/docker/vendor"))
+    (setenv "GOPATH" (concat gopath ":" vendor)))
+  )
