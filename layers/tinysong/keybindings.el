a3268c80ea87f242cbef1df315694845b8b848d2
tinysong <tinysong1226@gmail.com>
2017-05-06 17:11:28 +0800

    move some keybindings into 8ts

--- layers/tinysong/keybindings.el
+++ layers/tinysong/keybindings.el
@@ -35,3 +35,69 @@
 
 (bind-key* "C-c l" 'tinysong/insert-chrome-current-tab-url)
 (spacemacs/set-leader-keys "oil" 'tinysong/insert-chrome-current-tab-url)
+
+(global-set-key (kbd "<f5>") 'tinysong/run-current-file)
+
+(global-set-key (kbd "s-/") 'hippie-expand)
+
+;; A complementary binding to the apropos-command (C-h a)
+(define-key 'help-command "A" 'apropos)
+
+
+(define-key 'help-command (kbd "C-f") 'find-function)
+(define-key 'help-command (kbd "C-k") 'find-function-on-key)
+(define-key 'help-command (kbd "C-v") 'find-variable)
+(define-key 'help-command (kbd "C-l") 'find-library)
+
+(define-key 'help-command (kbd "C-i") 'info-display-manual)
+
+(global-set-key [(shift return)] 'smart-open-line)
+
+(define-key global-map (kbd "<f1>") 'zilongshanren/hotspots)
+
+;; (global-set-key (kbd "C-.") 'company-capf)
+
+
+;; some easy functions for navigate functions
+;;C-M-a beginning-of-defun
+;;C-M-e end-of-defun
+;;C-M-h mark-defun
+(global-set-key (kbd "C-s-h") 'mark-defun)
+
+(global-set-key (kbd "s-l") 'goto-line)
+(global-set-key (kbd "s-s") 'save-buffer)
+(global-set-key (kbd "C-`") 'toggle-input-method)
+
+;; "http://endlessparentheses.com/transposing-keybinds-in-emacs.html?source=rss"
+;; (global-set-key "\C-t" #'transpose-lines)
+;; (define-key ctl-x-map "\C-t" #'transpose-chars)
+
+(when (spacemacs/system-is-mac)
+  (spacemacs/set-leader-keys "o!" 'zilongshanren/iterm-shell-command))
+
+(spacemacs|add-toggle toggle-shadowsocks-proxy-mode
+  :status shadowsocks-proxy-mode
+  :on (global-shadowsocks-proxy-mode)
+  :off (global-shadowsocks-proxy-mode -1)
+  :documentation "Toggle shadowsocks proxy mode."
+  :evil-leader "ots")
+
+(global-set-key (kbd "s-s") 'save-buffer)
+(bind-key* "s-k" 'scroll-other-window-down)
+(bind-key* "s-j"  'scroll-other-window)
+(bind-key* "C-c /" 'company-files)
+
+(bind-key* "s-r" 'tinysong/browser-refresh--chrome-applescript)
+
+(bind-key* "s-;" 'zilongshanren/insert-semicolon-at-the-end-of-this-line)
+(bind-key* "C-s-;" 'zilongshanren/delete-semicolon-at-the-end-of-this-line)
+
+(bind-key* "s-," 'zilongshanren/insert-comma-at-the-end-of-this-line)
+(bind-key* "C-s-," 'zilongshanren/delete-comma-at-the-end-of-this-line)
+
+(bind-key* "C-=" 'er/expand-region)
+
+
+(bind-key* "M--" 'tinysong/goto-match-paren)
+
+(spacemacs/set-leader-keys "ol" 'tinysong/load-my-layout)
