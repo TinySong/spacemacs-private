4a5143215c5c9c58b8341b378beb3940c9f08bd4
tinysong <tinysong1226@gmail.com>
2017-06-14 19:52:29 +0800

    add confluence layer

--- init.el
+++ init.el
@@ -112,6 +112,7 @@ values."
      docker
      graphviz
      yaml
+     confluence
      ;; selectric
      ;; scheme
      ;; speed-reading
@@ -221,7 +222,7 @@ values."
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
    ;; size to make separators look not too crappy.
    dotspacemacs-default-font '("Source Code Pro"
-                               :size 14
+                               :size 13
                                :weight normal
                                :width normal
                                :powerline-scale 1.1)
