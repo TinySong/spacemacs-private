6705aadc5fa2c65585576d2e8ee0c87194b9e24b
timmysong <tinysong1226@gmail.com>
2017-08-01 23:04:56 +0800

    update fill-column number

--- layers/tinysong/config.el
+++ layers/tinysong/config.el
@@ -31,7 +31,7 @@
 
 
 (global-prettify-symbols-mode 1)
-(setq-default fill-column 110)
+;; (setq-default fill-column 110)
 
 (setq recenter-positions '(top middle bottom))
 ;; delete the selection with a key press
